eb778352b8f167e854f9a7e8aed9058c383a5b82
Aditya-711 <64785580+Aditya-711@users.noreply.github.com>
2025-09-03 18:41:59 +0530

    Add EC2 variables for region, AMI, and instance type

--- terraform/ec2/variables.tf
+++ terraform/ec2/variables.tf
@@ -1,1 +1,15 @@
+variable "region" {
+  default = "us-east-1"
+}
 
+variable "ami_id" {
+  default = "ami-020cba7c55df1f615" # Ubunt in us-east-1
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "instance_name" {
+  default = "MyEC2Instance"
+}
